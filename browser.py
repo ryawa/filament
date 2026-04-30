4506e2c7a9589fc887e1a4c03f6aebcb8354645e
ryawa <22625346+ryawa@users.noreply.github.com>
2026-04-30 18:15:08 -0400

    Implement max-age caching

--- browser.py
+++ browser.py
@@ -1,5 +1,21 @@
+from dataclasses import dataclass
+from pathlib import Path
+
+import datetime
+import io
 import socket
 import ssl
+
+BASE_DIR = Path(__file__).parent.resolve()
+CACHE_DIR = BASE_DIR / ".cache/"
+
+@dataclass
+class Response:
+    raw: str
+    status: int
+    headers: dict[str, str]
+    content: str
+
 
 class URL:
     sockets = {}
@@ -56,60 +72,109 @@ class URL:
                 with open(self.path, "r") as f:
                     content = f.read()
             case "http" | "https":
-                if (self.host, self.port) not in self.sockets:
-                    s = socket.socket(
-                            family=socket.AF_INET,
-                            type=socket.SOCK_STREAM,
-                            proto=socket.IPPROTO_TCP,
+                cached_path = CACHE_DIR / self.host / self.path.lstrip("/")
+                if cached_path.is_file():
+                    cached_response = URL._parse_http(
+                        io.BytesIO(cached_path.read_bytes())
                     )
-                    s.connect((self.host, self.port))
-                    if self.scheme == "https":
-                        ctx = ssl.create_default_context()
-                        s = ctx.wrap_socket(s, server_hostname=self.host)
-                    self.sockets[(self.host, self.port)] = s
+
+                    cache_directives = cached_response.headers["cache-control"].split(", ")
+                    max_age = None
+                    for directive in cache_directives:
+                        if directive.startswith("max-age="):
+                            max_age = int(directive.removeprefix("max-age="))
+                    assert max_age is not None, "Expected max-age value in the headers of a previously cached response"
+
+                    # TODO: make checking staleness compliant with RFC 9111
+                    response_time = cached_path.stat().st_mtime
+                    if datetime.datetime.now().timestamp() - response_time > max_age:
+                        content = self._request_http(request_headers)
+                    else:
+                        content = cached_response.content
                 else:
-                    s = self.sockets[(self.host, self.port)]
-
-                request = f"GET {self.path} HTTP/1.1\r\n"
-                request += f"Host: {self.host}\r\n"
-                for header, value in request_headers.items():
-                    request += f"{header}: {value}\r\n"
-                request += "\r\n"
-                s.send(request.encode("utf8"))
-
-                encoding = "utf8"
-                response = s.makefile("rb", newline="\r\n")
-                statusline = response.readline().decode(encoding)
-                version, status, explanation = statusline.split(" ", 2)
-
-                response_headers = {}
-                while True:
-                    line = response.readline().decode(encoding)
-                    if line == "\r\n":
-                        break
-                    header, value = line.split(":", 1)
-                    response_headers[header.casefold()] = value.strip()
-                assert "transfer-encoding" not in response_headers
-                assert "content-encoding" not in response_headers
-
-                # Redirect for 3xx status codes
-                # TODO: handle 304 correctly
-                if status.startswith("3") and status != "304":
-                    assert self.redirect_count < 20, "Too many consecutive redirects"
-                    location = response_headers["location"]
-                    if location.startswith("/"):
-                        location = f"{self.scheme}://{self.host}{location}"
-                    return URL(location, self.redirect_count + 1).request()
-
-                content = response.read(
-                        int(response_headers["content-length"])
-                ).decode(encoding)
+                    content = self._request_http(request_headers)
 
         if self.view_source:
             content = content.replace("<", "&lt;")
             content = content.replace(">", "&gt;")
 
         return content
+
+    def _request_http(self, request_headers):
+        if (self.host, self.port) not in self.sockets:
+            s = socket.socket(
+                    family=socket.AF_INET,
+                    type=socket.SOCK_STREAM,
+                    proto=socket.IPPROTO_TCP,
+            )
+            s.connect((self.host, self.port))
+            if self.scheme == "https":
+                ctx = ssl.create_default_context()
+                s = ctx.wrap_socket(s, server_hostname=self.host)
+            self.sockets[(self.host, self.port)] = s
+        else:
+            s = self.sockets[(self.host, self.port)]
+
+        request = f"GET {self.path} HTTP/1.1\r\n"
+        request += f"Host: {self.host}\r\n"
+        for header, value in request_headers.items():
+            request += f"{header}: {value}\r\n"
+        request += "\r\n"
+        s.send(request.encode("utf8"))
+
+        response_file = s.makefile("rb", newline="\r\n")
+        response = URL._parse_http(response_file)
+
+        # 304 => get from cache, other 3xx => redirect, other => return and
+        # optionally cache
+        if response.status == 304:
+            cached_path = CACHE_DIR / self.host / self.path.lstrip("/")
+            cached_response = URL._parse_http(
+                io.BytesIO(cached_path.read_bytes())
+            )
+            return cached_response.content
+        elif 300 <= response.status <= 399:
+            assert self.redirect_count < 20, "Too many consecutive redirects"
+            location = response.headers["location"]
+            if location.startswith("/"):
+                location = f"{self.scheme}://{self.host}{location}"
+            return URL(location, self.redirect_count + 1).request()
+        else:
+            cache_control = response.headers.get("cache-control", None)
+            if (response.status == 200 and cache_control is not None and
+                    "max-age" in cache_control and "no-store" not in cache_control):
+                cache_path = CACHE_DIR / self.host / self.path.lstrip("/")
+                cache_path.parent.mkdir(parents=True, exist_ok=True)
+                cache_path.write_text(response.raw)
+            return response.content
+
+
+    @staticmethod
+    def _parse_http(response_file):
+        encoding = "utf8"
+        statusline = response_file.readline().decode(encoding)
+        raw = statusline
+        version, status, explanation = statusline.split(" ", 2)
+        status = int(status)
+
+        headers = {}
+        while True:
+            line = response_file.readline().decode(encoding)
+            raw += line
+            if line == "\r\n":
+                break
+            header, value = line.split(":", 1)
+            headers[header.casefold()] = value.strip()
+        assert "transfer-encoding" not in headers
+        assert "content-encoding" not in headers
+
+        content = response_file.read(
+            int(headers["content-length"])
+        ).decode(encoding)
+        raw += content
+
+        return Response(raw, status, headers, content)
+
 
     @classmethod
     def close_sockets(cls):
@@ -154,6 +219,7 @@ if __name__ == "__main__":
         if len(sys.argv) == 2:
             load(URL(sys.argv[1]))
         else:
-            load(URL("file:///Users/ryan/code/filament/test.html"))
+            test_page = Path.cwd() / "test.html"
+            load(URL(f"file:///{test_page}"))
     finally:
         URL.close_sockets()
